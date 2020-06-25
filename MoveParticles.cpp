ca3b2264e7dabd5b519c85ebd93b98acdb47e66e
Frederik Engels <frederik.engels92@gmail.com>
2020-06-25 15:54:12 +0200

    add the omp simd directive

--- MoveParticles.cpp
+++ MoveParticles.cpp
@@ -57,6 +57,9 @@ void MoveParticlesOpt(const int nr_Particles, Particle *const partikel,
     float Fx = 0, Fy = 0, Fz = 0;
 
     // Schleife �ber die anderen Partikel die Kraft auf Partikel i aus�ben
+    // 5a) Erfordere eine Vektorisierung und gebe an das unser partikel 32 byte
+    // aligned ist
+#pragma omp simd simdlen(8) aligned(partikel : 32)
     for (int j = 0; j < nr_Particles; j++) {
 
       // Abschw�chung als zus�tzlicher Abstand, um Singularit�t und
@@ -72,16 +75,10 @@ void MoveParticlesOpt(const int nr_Particles, Particle *const partikel,
 
       // 3a) Strength reduction, sqrt is gunstiger zu berechnen als pow
       float drPower32 = std::sqrt(drSquared) * drSquared;
-      // const float drPower32 = pow(drSquared, 3.0 / 2.0);
       // 3a) einmaliges vorberechnen der inversen
       float invDrPower32 = 1.f / drPower32;
 
       // Addiere Kraftkomponenten zur Netto-Kraft
-      /*
-  Fx += dx / drPower32;
-  Fy += dy / drPower32;
-  Fz += dz / drPower32;
-      */
       // 3a) Hier wird die inverse jetzt benutzt um die kosten der Division zu
       // sparen
       Fx += dx * invDrPower32;
