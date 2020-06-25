2a736674ca1148ac5a9a04e9363c2727ba652155
Frederik Engels <frederik.engels92@gmail.com>
2020-06-25 16:28:00 +0200

    add soa data structure

--- main.h
+++ main.h
@@ -5,11 +5,22 @@
 #include <cstdio>
 #include <omp.h>
 
-
 // Struct zur Beschreibung eines Teilchens
 struct Particle {
   float x, y, z;    // Koordinaten des Teilchens
   float vx, vy, vz; // Geschwindigkeiten des Teilchens
+};
+
+// 5c)
+// Alle x, y, z, vx, vy, vz haben ihr eigenes array. Hierdurch koennen unit
+// strided access erzeugt werden.
+struct ParticleSoA {
+  float *__restrict__ x;
+  float *__restrict__ y;
+  float *__restrict__ z;
+  float *__restrict__ vx;
+  float *__restrict__ vy;
+  float *__restrict__ vz;
 };
 
 // Prototypen
